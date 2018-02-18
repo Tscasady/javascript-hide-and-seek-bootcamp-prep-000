e3c9aff25e583589b7335660b8f9144febe5148c
Thomas Casady <tscasady@gmail.com>
2018-02-18 23:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').getElementsByClassName('target')[0]
+  return document.getElementsByClassName('target')[0]
 }
 
 function increaseRankBy(n){
