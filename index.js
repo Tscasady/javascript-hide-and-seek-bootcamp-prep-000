213cb8d0512d738eaa7f5b0e41d8633c2799bf98
Thomas Casady <tscasady@gmail.com>
2018-02-19 00:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.getElementById('grand-node').querySelector(div div div div).innerHTML
+  return document.getElementById('grand-node').querySelector('div div div div')
 }
